a94a8feed5ddbf12c73bcc8f929cbc6648c18003
Nelly Ackerman <pavel.ack@gmail.com>
2018-07-17 14:56:20 -0400

    changing Project model

--- server/models/Project.js
+++ server/models/Project.js
@@ -3,10 +3,14 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var projectSchema = new Schema({
+  picture: String,
   title: String,
-  date: Date,
-  summary : String,
-  link: String
+  link: String,
+  category: String,
+  subtitle: String,
+  duration: String,
+  location: String,
+  note: String
 });
 
 var Project = mongoose.model("Project", projectSchema);
